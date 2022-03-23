dbc1fe14aa92bf221bebfa4360bb7b272e3dd13e
Anastasia Yaschenko <47624778+anastasia-yaschenko@users.noreply.github.com>
2022-03-23 08:03:04 +0300

    Update celeba (1).py

--- celeba (1).py
+++ celeba (1).py
@@ -42,7 +42,7 @@ class CelebADataset(Dataset):
                 download_url = 'https://drive.google.com/file/d/12agH1nWYcj7PAoErxQQgFdOAohgS9qE_/view?usp=sharing'
                 gdown.download(download_url, download_path, quiet=False, fuzzy=True)
             else:
-                download_url = 'https://drive.google.com/uc?id=1GFE43bkKYTZDAoisc9lgIjHSzluPQsK4'
+                download_url = 'https://drive.google.com/file/d/1GFE43bkKYTZDAoisc9lgIjHSzluPQsK4/view?usp=sharing'
                 gdown.download(download_url, download_path, quiet=False)
             # Unzip the downloaded file 
             with zipfile.ZipFile(download_path, 'r') as ziphandler:
